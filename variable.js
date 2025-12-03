394c7de5ecfe7bf066e3ad0eb1ab3e743cd5e0f2
Mohd Shaquib Ansari <229451865+shaquib2007@users.noreply.github.com>
2025-12-03 14:46:59 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,19 @@
+let x = 10    //  assignment operator
+    x = 5      // reassignment operator
+let b = "hello" 
+b = "hi"
+let n = "nnn"   // declaration and assignment //redeclaration is not allowed in let
+console.log(x)
+console.log(b)
+console.log(n)
+
+const y = 20   // declaration and assignment // reassignment and redeclaration is not allowed in const
+// y = 15   // error
+console.log(y)
+
+var z = 30    // declaration and assignment // redeclaration and reassignment is allowed in var
+z = 25
+var z = 40
+console.log(z) // output 40
+
+// for loop
